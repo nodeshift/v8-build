7633f45d76aa3e1de87fb11ef8d6e952df46966c
Milad Fa <mfarazma@redhat.com>
2022-08-29 15:56:05 -0400

    add abseil build scripts

---
+++ non_v8/abseil.sh
@@ -0,0 +1,10 @@
+if [[ -z "$NPROC" ]] ; then
+  NPROC=$(nproc)
+fi
+
+git clone https://github.com/abseil/abseil-cpp.git && cd abseil-cpp
+git checkout master
+mkdir build && cd build
+cmake -DABSL_BUILD_TESTING=ON -DBUILD_TESTING=ON -DABSL_USE_GOOGLETEST_HEAD=ON ..
+MAKEFLAGS="-j$NPROC" cmake --build . --target all
+ctest -j$NPROC
